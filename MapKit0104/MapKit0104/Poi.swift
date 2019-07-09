2296b6947d229f8969abc73845745ed45908e1c1
Grattours <grattours@gmail.com>
2019-07-09 18:14:32 +0200

    Enrichir les annotations avec des informations : les MKPinAnnotationView

---
+++ MapKit0104/MapKit0104/Poi.swift
@@ -0,0 +1,24 @@
+//
+//  Poi.swift
+//  MapKit0104
+//
+//  Created by Luc Derosne on 09/07/2019.
+//  Copyright © 2019 Luc Derosne. All rights reserved.
+//
+
+// Point d'intérêt
+
+import MapKit
+import UIKit
+
+class Poi: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
