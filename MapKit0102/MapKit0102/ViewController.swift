b6d38b77b15ceb22907b431310cb0256f61eb8fd
Grattours <grattours@gmail.com>
2019-07-09 17:28:20 +0200

    Centrage de la carte sur un point GPS  précis

--- MapKit0102/MapKit0102/ViewController.swift
+++ MapKit0102/MapKit0102/ViewController.swift
@@ -6,15 +6,28 @@
 //  Copyright © 2019 Luc Derosne. All rights reserved.
 //
 
+// 2  Centrer une carte au démarrage sur des coordonnées précises
+
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // San José Capitale du Costa Rica
+    var latitudeInit: Double = 9.998784
+    var longitudeInit: Double = -84.204007
+    var coordinateInit :  CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitudeInit, longitude: longitudeInit)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let span = MKCoordinateSpan(latitudeDelta: 3, longitudeDelta: 3)
+        let region = MKCoordinateRegion(center: coordinateInit, span: span)
+        mapView.setRegion(region, animated: true)
+        mapView.delegate = self
     }
-
-
+    
 }
-
